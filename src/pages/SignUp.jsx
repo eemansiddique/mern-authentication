c6aab47a342418b91e3c1f7cef0bc72316d8ab5e
eemansiddique <eeman@srvinfotech.in>
2024-01-12 22:32:43 +0530

    handle submit added

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,32 +3,71 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 // import OAuth from '../components/OAuth';
 function SignUp() {
+  const[formData,setFormData]=useState({})
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const handleChange=(e)=>{
+      setFormData({...formData,[e.target.id]:e.target.value})
+      console.log(e.target.value)
+  }
+  const handleSubmit= async (e) =>{
+   
+    e.preventDefault();
+    const res=await fetch('http://localhost:3000/api/auth/signup',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+    const data = await res.json();
+    console.log(data); 
+  }
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     const res = await fetch('/api/auth/signup', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify(formData),
+  //     });
+  //     if (!res.ok) {
+  //       throw new Error(`HTTP error! Status: ${res.status}`);
+  //     }
+  //     const data = await res.json();
+  //     console.log(data);
+  //   } catch (error) {
+  //     console.error('Error:', error.message);
+  //   }
+  // };
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form  className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           type='text'
           placeholder='Username'
           id='username'
           className='bg-slate-100 p-3 rounded-lg'
-         
+          onChange={handleChange}
+
         />
         <input
           type='email'
           placeholder='Email'
           id='email'
           className='bg-slate-100 p-3 rounded-lg'
-          
+          onChange={handleChange}
         />
         <input
           type='password'
           placeholder='Password'
           id='password'
           className='bg-slate-100 p-3 rounded-lg'
-         
+          onChange={handleChange}
+
         />
         <button
           disabled={loading}
